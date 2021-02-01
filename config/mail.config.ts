b0caba713e2c1b16160fb8aec93846a666e851c4
Tonic Milena <tonicmilena99@gmail.com>
2021-02-01 14:41:22 +0100

    Slanje email poruke nakon izvrsavanja porudzbine

---
+++ config/mail.config.ts
@@ -0,0 +1,11 @@
+export const MailConfig = {
+    hostname: 'smtp.gmail.com',
+    //username: 'google-username',
+    username: 'tonicmilena6@gmail.com',
+    //password: 'Secret-Password-1234',
+    password: 'ZaProjekat321',
+    //senderEmail: 'google-username@gmail.com',
+    senderEmail: 'tonicmilena99@gmail.com',
+
+    orderNotificationMail: 'tonicmilena99@gmail.com' // Ovde ce stici notifikacija kada neko napravi porudzbinu
+}
